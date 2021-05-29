988a14e663503f66fd04a8fe60530f51702591b9
Anurak Khophadung <anurak.k@phukhieo.ac.th>
2021-05-29 21:55:10 +0700

    Updates

---
+++ precache-manifest.7f9b79e56723deda1e26eed751f31c6e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef72d22a1c6e47083cdd",
+    "url": "/online-report-v1/static/css/main.a4650ce9.chunk.css"
+  },
+  {
+    "revision": "ef72d22a1c6e47083cdd",
+    "url": "/online-report-v1/static/js/main.ef72d22a.chunk.js"
+  },
+  {
+    "revision": "e5d51e65f751cc39bef4",
+    "url": "/online-report-v1/static/css/1.e148edd9.chunk.css"
+  },
+  {
+    "revision": "e5d51e65f751cc39bef4",
+    "url": "/online-report-v1/static/js/1.e5d51e65.chunk.js"
+  },
+  {
+    "revision": "53a972ccfcebdd638883",
+    "url": "/online-report-v1/static/js/runtime~main.53a972cc.js"
+  },
+  {
+    "revision": "788c600b5a2bd7b8e6d7c5903b7bb00d",
+    "url": "/online-report-v1/static/media/backgroundB.788c600b.png"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/online-report-v1/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "3ce7c28e36ff53eb317fdb56f8865396",
+    "url": "/online-report-v1/static/media/backgroundG.3ce7c28e.png"
+  },
+  {
+    "revision": "fcb4ecb1bd201fce7bec92d5491e131e",
+    "url": "/online-report-v1/static/media/backgroundP.fcb4ecb1.png"
+  },
+  {
+    "revision": "c94e43e3a2ee597ea587c43675c9953d",
+    "url": "/online-report-v1/static/media/pk.c94e43e3.png"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/online-report-v1/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/online-report-v1/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "a82c4c3bfe6cd8a9d57597071772d0c7",
+    "url": "/online-report-v1/static/media/Kanit-Regular.a82c4c3b.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/online-report-v1/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "9bec958d1bdbaf4f03bdf23557b4122f",
+    "url": "/online-report-v1/static/media/K2D-Regular.9bec958d.ttf"
+  },
+  {
+    "revision": "f69fdd6858deaa1e307594ad974d3979",
+    "url": "/online-report-v1/static/media/ChakraPetch-Regular.f69fdd68.ttf"
+  },
+  {
+    "revision": "49d082d0164cdf471f5e00b1fddb89f9",
+    "url": "/online-report-v1/static/media/Sarabun-Regular.49d082d0.ttf"
+  },
+  {
+    "revision": "16b4ce72cf30da14d83d5e3981d3113c",
+    "url": "/online-report-v1/static/media/Prompt-Regular.16b4ce72.ttf"
+  },
+  {
+    "revision": "81b5837e530ab2ad3dfe555368f77336",
+    "url": "/online-report-v1/static/media/background.81b5837e.png"
+  },
+  {
+    "revision": "f678915a6d30cc90272f0f495584d8c2",
+    "url": "/online-report-v1/index.html"
+  }
+];
